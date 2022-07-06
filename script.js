152233fd985817025d4f1edb161c027b5ae429cd
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-07-06 12:11:55 -0400

    Start player select and display marker logic

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+//factory function to create player objects
+const player = (name, marker) => {
+    const assignedMarker = marker;
+    const placeMarker = (row, col) => {
+        gameBoard.markBox(row, col, assignedMarker);
+    };
+
+    return { placeMarker };
+};
+
 //game board object using module pattern
 const gameBoard = (() => {
+    
     const boardArray = [
-        ["X", "X", "O"],
-        ["X", "O", "X"],
-        ["O", "O", "X"] 
+        ["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"] 
+        // ["X", "X", "O"],
+        // ["X", "O", "X"],
+        // ["O", "O", "X"] 
     ];
 
-    const markBox = (x, y, marker) => boardArray[x][y] = marker;
+    const markBox = (row, col, marker) => boardArray[row][col] = marker;
     const displayOnScreen = () => {
         for (let i = 0; i < boardArray.length; i++){
             for (let j = 0; j < boardArray.length; j++){
@@ -23,15 +37,47 @@ const gameBoard = (() => {
 
 //display controller to handle flow of game using module pattern
 const displayController = (() => {
+    gameBoard.displayOnScreen();
+    const dee = player ("d", "X");
+
+    const displayBlocks = document.querySelectorAll(".board-row .board-box");
+    displayBlocks.forEach(block => {
+        block.addEventListener('click', () => {
+            //do logic here;
+            let rowClass = block.parentElement.classList[1];
+            let colClass = block.classList[1];
+            let row, col;
+
+            
+            //transform className to true array row pos value
+            switch(rowClass) {
+                case "row-1":
+                    row = 0;
+                    break;
+                case "row-2":
+                    row = 1;
+                    break;
+                case "row-3":
+                    row = 2;
+                    break;
+            }
+            //transform className to true array col pos value
+            switch(colClass) {
+                case "col-1":
+                    col = 0;
+                    break;
+                case "col-2":
+                    col = 1;
+                    break;
+                case "col-3":
+                    col = 2;
+                    break;
+            }
+
+            console.log(`row: ${row} col: ${col}`)
+            dee.placeMarker(row, col);
+            gameBoard.displayOnScreen();
+        });
+    });
 
 })();
-
-//factory function to create player objects
-const player = (name, marker) => {
-    const assignedMarker = marker;
-    const placeMarker = (x, y) => {
-        gameBoard.markBox(x, y, assignedMarker);
-    };
-
-    return { placeMarker };
-};
