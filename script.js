f7a1cd8cc096a7017c5b13505666424a83ffa1c1
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-07-12 16:30:22 -0400

    Refactor and cleanup code

--- script.js
+++ script.js
@@ -7,25 +7,40 @@
 
 //factory function to create player objects
 const player = (name, marker) => {
-    const assignedMarker = marker;
+
+    const playerMarker = marker;
+    
+    const getName = () => name;
+
+    const getMarker = () => playerMarker;
+
     const placeMarker = (row, col) => {
-        gameBoard.markBox(row, col, assignedMarker);
+        gameBoard.markBox(row, col, playerMarker);
     };
 
-    const getName = () => name;
-    const getMarker = () => assignedMarker;
+    
 
-    return { placeMarker, getName, getMarker };
+    return { getName, getMarker, placeMarker };
 };
 
 //game board object using module pattern
 const gameBoard = (() => {
     
     let boardArray = [
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"] 
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""] 
     ];
+
+    const getBoardArray = () => boardArray;
+
+    const resetArray = () => {
+        boardArray = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+    };
 
     const isFull = () =>{
         let arrayIsFull = true;
@@ -35,79 +50,48 @@ const gameBoard = (() => {
         //and assigning to value to box position on the display
         const stringArray = boardArrayString.split(',');
         for (let i = 0; i < stringArray.length; i++){
-            if (stringArray[i] === "-"){
+            if (stringArray[i] === ""){
                 arrayIsFull = false;
             }  
         }
         return arrayIsFull;     
     };
-    const getBoardArray = () => boardArray;
+    
     const markBox = (row, col, marker) => boardArray[row][col] = marker;
-    const displayOnScreen = () => {
-        //convert our 2d game board into a string seperated by commas
-        const boardArrayString = boardArray.toString();
-        //convert the string seperated by commas into a 1d array for looping  
-        //and assigning to value to box position on the display
-        const stringArray = boardArrayString.split(',');
-        let boxList = document.querySelectorAll('.board-box');
-        for (let i = 0; i < stringArray.length; i++){
-            boxList[i].textContent = stringArray[i];
-        }      
-    };
-    const resetArray = () => {
-        boardArray = [
-            ["-", "-", "-"],
-            ["-", "-", "-"],
-            ["-", "-", "-"]
-        ];
-    };
-
-    return { markBox, displayOnScreen, getBoardArray, resetArray, isFull};
+    
+    return { getBoardArray, resetArray, isFull, markBox };
 })();
 
 //game controller to handle flow of game using module pattern
 const gameController = (() => {
-
+    let gameOver;
     let playerOne;
     const opponent = player ("Opponent", "O");
     let playerTurn = 1;
 
-    
-    //
-    //
-    //event listeners
-    //
-    //
-    const startButton = document.querySelector(".welcome-pop-up button");
-    startButton.addEventListener('click', () => {
-        let playerName = document.querySelector(".welcome-pop-up input").value;
+    const startGame = () => {
+        let playerName = document.querySelectorAll(".welcome-pop-up input");
         if (playerName === ""){
             playerName = "Guest";
         }
         playerOne = player (playerName, "X");
-        showGame();
-        gameBoard.displayOnScreen(); //display starting board
-        hideWelcomePopUp();
-    });
+        document.getElementById("player-one-marker").textContent = playerOne.getMarker();
+        document.getElementById("player-name").textContent = playerOne.getName().toUpperCase();
+        displayController.showGame();
+        displayController.displayBoardOnScreen();
+        displayController.hideWelcomePopUp();
+    };
 
-    const playAgainButton = document.querySelector(".victory-pop-up button");
-    playAgainButton.addEventListener('click', () => {
-        gameBoard.resetArray();
-        hideVictoryPopUp();
-        gameBoard.displayOnScreen();
-    });
-
-    const displayBlocks = document.querySelectorAll(".board-box");
-    displayBlocks.forEach(block => {
-        block.addEventListener('click', () => {
-            //cannot select a spot on the board that has already been marked
-            
-            if (block.textContent !== "X" && block.textContent !== "O"){
+    const selectBoardSpot = (block) => {
+        
+        if (!gameOver){
+            //selected an empty block
+            if (block.target.textContent !== "X" && block.target.textContent !== "O"){
                 //do logic here;
-                let rowClass = block.classList[1];
-                let colClass = block.classList[2];
+                let rowClass = block.target.classList[1];
+                let colClass = block.target.classList[2];  
                 let row, col;
-
+    
                 //transform className to true array row pos value
                 switch(rowClass) {
                     case "row-1":
@@ -132,43 +116,137 @@ const gameController = (() => {
                         col = 2;
                         break;
                 }
-                
-                playTurn(row, col, block);
-                
-                gameBoard.displayOnScreen();
-                //check for a tie
-                if(gameBoard.isFull()){
-                    document.querySelector(".victory-pop-up h1").textContent = "IT'S A TIE";
-                    showVictoryPopUp();
-                }
-                //check for a winner
-                let winningPlayer = checkForWinner(block, row, col);
-                //winner found
-                if (winningPlayer !== undefined){
-                    document.querySelector(".victory-pop-up h1").textContent = `${winningPlayer.getName()} WINS!!!!`;
-                    showVictoryPopUp();
-                }
-                
-                
-                
+                playTurn(block, row, col);
+            }
+        }
+    };
+
+    const playTurn = (block, row, col) => {
+        if (playerTurn === 1){
+            playerOne.placeMarker(row, col);
+            if (checkForWinner(playerOne, row, col)){
+                playerTurn = 1;
             }
             else{
-                alert("Spot is already selected. Please choose a different spot");
+                playerTurn = 2;
+            }  
+        }
+        else {
+            opponent.placeMarker(row, col);
+            if (checkForWinner(opponent, row, col)){
+                playerTurn = 2;
             }
-        });
+            else{
+                playerTurn = 1;
+            }   
+        }
+        displayController.displayBoardOnScreen();
+    };
+
+    const checkForWinner = (player, row, col) => {
+
+        const boardArrayCopy = gameBoard.getBoardArray();
+        let rowsMatch = true;
+        
+        //check for 3 in a row
+        for (let i = 0; i < 3; i++){
+            if (boardArrayCopy[row][i] !== player.getMarker()){
+                rowsMatch = false;
+                break; 
+            }
+        }
+        if (rowsMatch){
+            console.log("victory by rows");
+            document.querySelector(".victory-pop-up h1").textContent = `${player.getName()} WINS!!!!`;
+            gameOver = true;
+            displayController.showVictoryPopUp();
+            return true;
+        }
+
+        let colsMatch = true;
+
+        //check for 3 in a column
+        for (let i = 0; i < 3; i++){
+            if (boardArrayCopy[i][col] !== player.getMarker()){
+                colsMatch = false;
+                break;
+            }
+        }
+        if (colsMatch){
+            console.log("victory by cols");
+            document.querySelector(".victory-pop-up h1").textContent = `${player.getName()} WINS!!!!`;
+            gameOver = true;
+            displayController.showVictoryPopUp();
+            return true;
+        }
+
+        
+        //check for 3 in upper left diagnol
+        if (boardArrayCopy[0][0] === player.getMarker() && boardArrayCopy[1][1] === player.getMarker() && boardArrayCopy[2][2]  === player.getMarker()){
+            console.log("victory by top left diag");
+            document.querySelector(".victory-pop-up h1").textContent = `${player.getName()} WINS!!!!`;
+            gameOver = true;
+            displayController.showVictoryPopUp();
+            return true;
+        }
+
+        //check for 3 in upper right diagnol
+        else if (boardArrayCopy[2][0] === player.getMarker() && boardArrayCopy[1][1] === player.getMarker() && boardArrayCopy[0][2]  === player.getMarker()){
+            console.log("victory by top right diag");
+            document.querySelector(".victory-pop-up h1").textContent = `${player.getName()} WINS!!!!`;
+            gameOver = true;
+            displayController.showVictoryPopUp();
+            return true;
+        } 
+
+        //check for a tie
+        if(gameBoard.isFull()){
+            document.querySelector(".victory-pop-up h1").textContent = "IT'S A TIE";
+            gameOver = true;
+            displayController.showVictoryPopUp();
+        }
+    }
+
+    const playAgain = () => {
+        gameOver = false;
+        gameBoard.resetArray();
+        displayController.hideVictoryPopUp();
+        displayController.displayBoardOnScreen();
+    };
+
+    return { startGame, playAgain, selectBoardSpot };
+
+})();
+
+
+
+const displayController = (() => {
+
+    const startButton = document.querySelector(".welcome-pop-up button");
+    startButton.addEventListener('click', gameController.startGame);
+
+    const playAgainButton = document.querySelector(".victory-pop-up button");
+    playAgainButton.addEventListener('click', gameController.playAgain);
+
+    const displayBlocks = document.querySelectorAll(".board-box");
+    displayBlocks.forEach(block => {
+        block.addEventListener('click', gameController.selectBoardSpot);
     });
 
-    //
-    //
-    // helper functions
-    //
-    //
+    const displayBoardOnScreen = () => {
+        //convert our 2d game board into a string seperated by commas
+        const boardArrayString = gameBoard.getBoardArray().toString();
+        //convert the string seperated by commas into a 1d array for looping  
+        //and assigning to value to box position on the display
+        const stringArray = boardArrayString.split(',');
+        let boxList = document.querySelectorAll('.board-box');
+        for (let i = 0; i < stringArray.length; i++){
+            boxList[i].textContent = stringArray[i];
+        }      
+    };
 
     const showGame = () => {
         document.querySelector(".wrapper").style.display = "flex";
-        document.getElementById("player-one-marker").textContent = playerOne.getMarker();
-        document.getElementById("player-name").textContent = playerOne.getName().toUpperCase();
-
     };
 
     const hideGame = () => {
@@ -191,83 +269,6 @@ const gameController = (() => {
         document.querySelector(".victory-pop-up").style.display = "none";
     };
 
-    const playTurn = (row, col) => {
-        if (playerTurn === 1){
-            playerOne.placeMarker(row, col);
-            playerTurn = 2;
-        }
-        else {
-            opponent.placeMarker(row, col);
-            playerTurn = 1;
-        } 
-    };
-
-    const checkForWinner = (block, row, col) => {
-
-        const blockVal = block.textContent;
-        const boardArrayCopy = gameBoard.getBoardArray();
-
-        let rowsMatch = true;
-        
-        //check for 3 in a row
-        for (let i = 0; i < 3; i++){
-            if (boardArrayCopy[row][i] !== blockVal){
-                rowsMatch = false;
-                break; 
-            }
-        }
-        if (rowsMatch){
-            console.log("victory by rows");
-            if (blockVal === playerOne.getMarker()){
-                return playerOne;
-            }
-            else{
-                return opponent;
-            }
-        }
-
-        let colsMatch = true;
-
-        //check for 3 in a column
-        for (let i = 0; i < 3; i++){
-            if (boardArrayCopy[i][col] !== blockVal){
-                colsMatch = false;
-                break;
-            }
-        }
-        if (colsMatch){
-            console.log("victory by cols");
-            if (blockVal === playerOne.getMarker()){
-                return playerOne;
-            }
-            else{
-                return opponent;
-            }
-        }
-
-        //check for 3 in a diagnol
-        if (boardArrayCopy[0][0] !== "-" && boardArrayCopy[1][1] !== "-" && boardArrayCopy[2][2]  !== "-"){
-            if (boardArrayCopy[0][0] === boardArrayCopy[1][1] && boardArrayCopy[1][1]  === boardArrayCopy[2][2]){
-                console.log("victory by top left diag");
-                if (blockVal === playerOne.getMarker()){
-                    return playerOne;
-                }
-                else{
-                    return opponent;
-                }
-            }
-        }
-        else if (boardArrayCopy[2][0] !== "-" && boardArrayCopy[1][1] !== "-" && boardArrayCopy[0][2]  !== "-"){
-            if (boardArrayCopy[2][0] === boardArrayCopy[1][1] && boardArrayCopy[1][1]  === boardArrayCopy[0][2]){
-                console.log("victory by top right diag");
-                if (blockVal === playerOne.getMarker()){
-                    return playerOne;
-                }
-                else{
-                    return opponent;
-                }
-            }
-        }   
-    };
+    return { displayBoardOnScreen, showGame, hideGame, showWelcomePopUp, hideWelcomePopUp, showVictoryPopUp, hideVictoryPopUp }
 
 })();
